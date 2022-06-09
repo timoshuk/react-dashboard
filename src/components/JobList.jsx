682ebfef2e965405e0435d8a69d65337c02e2887
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2022-06-09 16:55:57 +0300

    finish JobList

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -2,10 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { JobPosition } from "./JobPosition";
 
-import {
-  selectAllPositions,
-  selectVisiblePositions,
-} from "../store/positions/position.selectors";
+import { selectVisiblePositions } from "../store/positions/position.selectors";
 
 import { addFilter } from "store/filters/filter-actions";
 import { selectFilters } from "store/filters/filter-selectors";
